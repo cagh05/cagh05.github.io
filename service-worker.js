2b1251935230ea81a757bfe50ab60d6045bf70ea
Carlos Gómez <caragomezher@unal.edu.co>
2021-09-26 22:14:47 -0500

    Test sw

--- service-worker.js
+++ service-worker.js
@@ -40,15 +40,16 @@ async function onInstall(event) {
   console.info("Service worker: Install");
 
   // Fetch and cache all matching items from the assets manifest
-  const assetsRequests = self.assetsManifest.assets
-    .filter((asset) =>
-      offlineAssetsInclude.some((pattern) => pattern.test(asset.url))
-    )
-    .filter(
-      (asset) =>
-        !offlineAssetsExclude.some((pattern) => pattern.test(asset.url))
-    )
-    .map((asset) => new Request(asset.url));
+  // const assetsRequests = self.assetsManifest.assets
+  //   .filter((asset) =>
+  //     offlineAssetsInclude.some((pattern) => pattern.test(asset.url))
+  //   )
+  //   .filter(
+  //     (asset) =>
+  //       !offlineAssetsExclude.some((pattern) => pattern.test(asset.url))
+  //   )
+  //   .map((asset) => new Request(asset.url));
+  const assetsRequests = {}
   await caches.open(cacheName).then((cache) => cache.addAll(assetsRequests));
 }
 
